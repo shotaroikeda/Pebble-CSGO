91322eb693d93d21b54d464d6fd704d22d90f02e
shotaroikeda <ikeda.shot@gmail.com>
2016-01-13 14:14:11 -0800

    added js file

---
+++ Pebble-CSGO/src/js/pebble-app.js
@@ -0,0 +1,8 @@
+////////////////////////////////
+// Event Listeners for pebble //
+////////////////////////////////
+
+Pebble.addEventListener('showConfiguration', function (e) {
+    // Show config page
+    Pebble.openURL('http://')
+})
