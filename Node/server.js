6aba8a9ecab23088918c270c8797a1b50e8d68bf
shotaroikeda <ikeda.shot@gmail.com>
2016-01-14 23:01:49 -0800

    finished basic json endpoint

--- Node/server.js
+++ Node/server.js
@@ -1,32 +1,72 @@
 var http = require('http');
 
-port = 3000;
-host = '127.0.0.1';
- 
+///////////////////////////////////////////
+// Prerequisites for running the server. //
+///////////////////////////////////////////
+
+// Obtain local ip address to create an appropriate server.
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+var myHost = null;
+
+Object.keys(ifaces).forEach(function (ifname) {
+    ifaces[ifname].forEach(function (iface) {
+        if (iface.internal || iface.family == 'IPv6')
+            return;
+        else
+            myHost = iface.address;
+    });
+});
+
+// TODO: Find an open port to use
+port = 8099;
+
+var dataObj = null;
+
 server = http.createServer( function(req, res) {
- 
-    if (req.method == 'POST') {
-        console.log("Handling POST request...");
+    if (req.method == 'POST')
+    {
         res.writeHead(200, {'Content-Type': 'text/html'});
- 
+
         var body = '';
         req.on('data', function (data) {
             body += data;
         });
+
         req.on('end', function () {
-            console.log("POST payload: " + body);
-        	res.end( '' );
+            dataObj = JSON.parse(body);
         });
     }
     else
     {
-        console.log("Not expecting other request types...");
-        res.writeHead(200, {'Content-Type': 'text/html'});
-		var html = '<html><body>HTTP Server at http://' + host + ':' + port + '</body></html>';
-        res.end(html);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+
+        if (dataObj)
+        {
+            res.end(JSON.stringify(dataObj));
+        }
+        else
+        {
+            res.end(JSON.stringify({ data: null }));
+        }
     }
- 
 });
- 
-server.listen(port, host);
-console.log('Listening at http://' + host + ':' + port);
+
+try {
+    // Listen to both external ips and internal ips
+    // External ip used by Device connected with Pebble
+    server.listen(port, myHost);
+
+    // Internal ip used by CSGO to connect.
+    server.listen(port);
+
+    // Verbose statements
+    console.log('Listening at http://' + myHost + ':' + port);
+    console.log('Please go to the pebble watch settings and insert: http://' + myHost + ':' + port);
+}
+catch (e)
+{
+    console.log('Could not obtain an open port at ' + port + '. Please try again.');
+}
