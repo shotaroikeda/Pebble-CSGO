c6ec6ee98bf13204a3418a86dbc4621cb4fe7aae
shotaroikeda <ikeda.shot@gmail.com>
2016-01-29 05:59:09 -0600

    preparation for AppMessage

--- Pebble-CSGO/src/Pebble-CSGO.c
+++ Pebble-CSGO/src/Pebble-CSGO.c
@@ -1,60 +1,77 @@
 #include <pebble.h>
 
+static bool valid_server_ip = false;
+
 static Window *window;
 static TextLayer *text_layer;
+static TextLayer *t_verbose;
+
+static AppSync s_sync;
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+        text_layer_set_text(text_layer, "Select");
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+        text_layer_set_text(text_layer, "Up");
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+        text_layer_set_text(text_layer, "Down");
 }
 
 static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+        window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+        window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+        window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+        Layer *window_layer = window_get_root_layer(window);
+        GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+        text_layer = text_layer_create((GRect) { .origin = { 0, 36 }, .size = { bounds.size.w, 20 } });
+        t_verbose = text_layer_create((GRect) { .origin = { 0, 108 }, .size = { bounds.size.w, 20 } });
+        text_layer_set_text(text_layer, "Press a button");
+
+        valid_server_ip ?
+                text_layer_set_text(t_verbose, "All is good.") :
+                text_layer_set_text(t_verbose, "No Valid server found.");
+
+        text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+	text_layer_set_text_alignment(t_verbose, GTextAlignmentCenter);
+
+        layer_add_child(window_layer, text_layer_get_layer(text_layer));
+	layer_add_child(window_layer, text_layer_get_layer(t_verbose));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+        text_layer_destroy(text_layer);
+        text_layer_destroy(t_verbose);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+        window = window_create();
+        window_set_click_config_provider(window, click_config_provider);
+        window_set_window_handlers(window, (WindowHandlers) {
+                        .load = window_load,
+                                .unload = window_unload,
+                                });
+        const bool animated = true;
+        window_stack_push(window, animated);
+
+        // Create the configuration buffer
 }
 
 static void deinit(void) {
-  window_destroy(window);
+        window_destroy(window);
 }
 
 int main(void) {
-  init();
+        init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
-  app_event_loop();
-  deinit();
+        app_event_loop();
+        deinit();
 }
--- Pebble-CSGO/src/js/pebble-js-app.js
+++ Pebble-CSGO/src/js/pebble-js-app.js
@@ -46,5 +46,48 @@ Pebble.addEventListener('webviewclosed', function (e) {
 /////////////////////////////////
 
 function retrieveCSGO() {
-    
+    if (!server_ip) {
+        // Bad Initial Configuration
+        console.log("Server's IP Address is not properly configured!");
+        Pebble.sendAppMessage({
+            'CSGO_VALID_SERVER_IP': 0,
+            'CSGO_TIME_SINCE_LAST_UPDATE': 0,
+            'CSGO_MAP_MODE': "none",
+            'CSGO_ROUND_PHASE': "none",
+            'CSGO_BOMB_STATUS': "none"
+        });
+        return;
+    }
+
+    var req = new XMLHttpRequest();
+    req.open('GET', server_ip);
+    req.onload = function () {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                console.log(req.responseText);
+
+                // CSGO's Json object is changed around in here.
+                var csgo_data = JSON.parse(req.responseText);
+
+                // MARK: Time diff might be screwed up.
+                var timeDiff = (new Date()).getTime() - csgo_data.provider["timestamp"];
+                console.log("Time difference: " + timeDiff);
+
+                var csgo_curr_gamemode = csgo_data.map["mode"];
+                console.log("Current GameMode: " + csgo_curr_gamemode);
+                var csgo_round_phase = csgo_data.round["phase"];
+                console.log("Current Phase: " + csgo_round_phase);
+                var csgo_bomb_status = csgo_data.round["bomb"];
+                console.log("Current Bomb Status: " + csgo_bomb_status);
+
+                Pebble.sendAppMessage({
+                    'CSGO_VALID_SERVER_IP': 1,
+                    'CSGO_TIME_SINCE_LAST_UPDATE': timeDiff,
+                    'CSGO_MAP_MODE': csgo_curr_gamemode,
+                    'CSGO_ROUND_PHASE': csgo_round_phase,
+                    'CSGO_BOMB_STATUS': csgo_bomb_status
+                });
+            }
+        }
+    };
 }
